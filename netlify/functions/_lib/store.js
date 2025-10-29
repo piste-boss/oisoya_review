d29e74f7fe489ed0aac14213c62b3c7c50e4af1d
piste-boss <s.ishikawa@piste-i.com>
2025-10-29 17:34:47 +0900

    Use object-based store initialization for Blobs

--- netlify/functions/_lib/store.js
+++ netlify/functions/_lib/store.js
@@ -2,7 +2,7 @@ import { getStore } from '@netlify/blobs'
 
 const DEFAULT_STORE_NAME = 'oiso-review-router-config'
 
-const resolveStoreOptions = () => {
+const getCredentials = () => {
   const siteID = process.env.NETLIFY_SITE_ID || process.env.BLOBS_SITE_ID
   const token =
     process.env.NETLIFY_BLOBS_TOKEN ||
@@ -13,15 +13,18 @@ const resolveStoreOptions = () => {
     return null
   }
 
-  const options = {}
-  if (siteID) options.siteID = siteID
-  if (token) options.token = token
-  return options
+  return { siteID, token }
 }
 
 export const createStore = (name = DEFAULT_STORE_NAME) => {
-  const options = resolveStoreOptions()
-  return options ? getStore(name, options) : getStore(name)
+  const credentials = getCredentials()
+  if (credentials) {
+    return getStore({
+      name,
+      ...credentials,
+    })
+  }
+  return getStore({ name })
 }
 
 export const getConfigStore = () => createStore()
